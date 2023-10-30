3cbd86210cce30cc3e833fd7be850bb54efcea5a
Kyoudai Ken <bloodysword@gmx.net>
2023-10-30 22:52:45 +0100

    I'm getting somewhere, but it's messy

--- core.js
+++ core.js
@@ -27,6 +27,7 @@ var remotehwinfo_req = function(method, url) {
                 statusText: xhr.statusText
             });
         };
+        xhr.setRequestHeader("Content-Type", "text/html; charset=UTF-8");
         xhr.send();
     });
 }
@@ -38,10 +39,44 @@ var get_sensors = function() {
         data = data.replaceAll(": inf", ": \"Infinity\"");
         data = JSON.parse(data);
         console.log(data);
+
+        document.getElementById("setup-add-sensor-wnd").style.visibility = "visible";
+
+        //Create a list of sensors and readings
+        var tree = document.getElementById("sensor-tree");
+        tree.innerHTML = "";
+        for (let si = 0; si < data.hwinfo.sensors.length; si++) {
+            var dom_li = document.createElement("li");
+            dom_li.insertAdjacentHTML("beforeend","<div class=\"caret\">"+data.hwinfo.sensors[si].sensorNameUser+"</div>");
+            var dom_ul_top = document.createElement("ul")
+            dom_ul_top.classList.toggle("nested");
+            for (let ri = 0; ri < data.hwinfo.readings.length; ri++) {
+                if(data.hwinfo.readings[ri].sensorIndex == data.hwinfo.sensors[si].entryIndex) {
+                    var dom_ul_li = document.createElement("li");
+                    dom_ul_li.insertAdjacentHTML("beforeend","<div>"+data.hwinfo.readings[ri].labelUser+" | unit: "+data.hwinfo.readings[ri].unit+"</div>")
+                    dom_ul_top.appendChild(dom_ul_li);
+                }
+                dom_li.appendChild(dom_ul_top);
+            }
+            tree.appendChild(dom_li);
+        }
+
+        //Collapse
+        var toggler = document.getElementsByClassName("caret");
+        for (var i = 0; i < toggler.length; i++) {
+            toggler[i].addEventListener("click", function() {
+                this.parentElement.querySelector(".nested").classList.toggle("active");
+                this.classList.toggle("caret-down");
+            });
+        } 
     })
     .catch(function (err) {
         console.error('Augh, there was an error!', err.statusText);
     });
+}
+
+var close_add_sensor_btn_click = function() {
+    document.getElementById("setup-add-sensor-wnd").style.visibility = "hidden";
 }
 
 var add_sensor_btn_click = function() {
